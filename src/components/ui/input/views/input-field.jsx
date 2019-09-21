02d3c36ebad02a6faa1f67d101842618a72cbe37
JL <jesper.lindholmer@avaloninnovation.com>
2019-09-21 15:12:23 +0200

    Fixed settings in local dev.

--- src/components/ui/input/views/input-field.jsx
+++ src/components/ui/input/views/input-field.jsx
@@ -1,20 +1,21 @@
-import react from "react";
-import propTypes from "prop-types";
+import React from 'react';
+import propTypes from 'prop-types';
 
-const InputField = (props) => {
-    const { type } = props;
-    return <input type={type} value={value} />
-}
+const InputField = props => {
+  const { types, value } = props;
+  return <input type={types} value={value} />;
+};
 
 InputField.defaultProps = {
-    type: "text",
-    value: ""
-}
-Input.propTypes = {
-    /** Set what type of input that should be used. */
-    type: propTypes.string,
-    /** What value the fields has.  */
-    value: propTypes.string
-}
+  types: 'text',
+  value: ''
+};
+InputField.propTypes = {
+  /** Set what type of input that should be used. */
+  types: propTypes.string,
+
+  /** What value the fields has.  */
+  value: propTypes.object
+};
 
 export default InputField;
