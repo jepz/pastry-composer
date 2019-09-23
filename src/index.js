cedb13212a889a9172ecc0f111f3d6ef1af6b7ec
Jesper L <jesper.lindholmer@avaloninnovation.com>
2019-09-23 18:23:46 +0200

    Added login to be used before app.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Login from './Login';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Login />, document.getElementById('root'));
 
 serviceWorker.unregister();
